27da9e3662ec8c5c9cd72c109a39a2a0a27f5464
Admin <admin@taga.io>
2015-12-23 15:52:11 +0530

    Basic directory structure created

---
+++ src/main.c
@@ -0,0 +1,35 @@
+/* src/main.c */
+
+/*
+ *	Copyright (C) 2015, Goutam-labs.taga.io
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful, but
+ *	WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *	General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program. If not, email to admin@taga.io.
+ */
+
+/**
+ * This is the entry point of the whole application.
+ * @file main.c
+ * @author Goutam-labs.taga.io
+ * @date Wed Dec 10:29:30 2015
+ */
+
+/*
+ * THE MAIN FUNCTION
+ */
+int 
+main(int argc, char *argv[])
+{
+
+	return 0;
+}
